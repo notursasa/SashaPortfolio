5e67fc6d390440e1d750d45e8c1039dbf785982a
StuntLead <fernandodarren@gmail.com>
2026-05-21 14:19:15 +0700

    Update profile photo to sasha_portrait.png

--- src/components/ScrapbookAbout.jsx
+++ src/components/ScrapbookAbout.jsx
@@ -19,7 +19,7 @@ const ScrapbookAbout = () => {
           <div className="col-12 col-md-5">
             <div className="card" style={{ padding: '16px 16px 48px', backgroundColor: '#fff', transform: 'rotate(-2deg)' }}>
               <img 
-                src="/newElements/my photo.jpg" 
+                src="/images/sasha_portrait.png" 
                 alt="Aisyah Sasha Raissa Algebra" 
                 style={{ width: '100%', height: 'auto', borderRadius: 'var(--radius-sm)' }}
               />
